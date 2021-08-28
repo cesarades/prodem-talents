fce26bc4251286cea104dc5d50fe7a59881ec0c4
Cesar Ades <kito@ades.as>
2021-08-28 18:22:27 +0200

    added show (confirmation) route

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -5,7 +5,22 @@ class MeetingsController < ApplicationController
     @meetings = Meeting.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 
-  def create
+  def show
+  end
 
+  def create
+    @meeting = Meeting.new(meeting_params)
+    @meeting.user = current_user
+    if @meeting.save
+      redirect_to meeting_path(@meeting)
+    else
+      render "meetings"
+    end
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:agent, :start_time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
-  resources :meetings, only: %i[index create]
+  resources :meetings, only: %i[index show create]
 end
