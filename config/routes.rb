8bd0839761e431854b0102ffc518d4d4997d9dc3
Cesar Ades <kito@ades.as>
2021-08-28 08:28:18 +0100

    Added a schedule page (route, action, view)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
+  get 'schedule', to: 'pages#schedule', as: :schedule
 end
